f14baeb33bb6905c027d3b40e0f0e754acac4bf8
Arno Lambert <arno.lambert@gmail.com>
2014-09-18 16:51:43 +0200

    Create bcm2708.h
    first commit
    header file that will be needed to access the RasPi register, lowlevel

---
+++ bcm2708.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+ 
+#include <unistd.h>
+
+//this is where the register for RasPi begins
+#define BCM2708_PERI_BASE       0x20000000
+//start of the register for the GPIO pins
+#define GPIO_BASE               (BCM2708_PERI_BASE + 0x200000)
+ 
+#define BLOCK_SIZE 		(4*1024)
+ 
+// IO Acces
+struct bcm2835_peripheral {
+    unsigned long addr_p;
+    int mem_fd;
+    void *map;
+    volatile unsigned int *addr;
+};
+ 
+struct bcm2835_peripheral gpio = {GPIO_BASE};
+ 
+extern struct bcm2835_peripheral gpio;  // They have to be found somewhere, but can't be in the header
