63f761c55abc4927b733b9a2265acb8c97db1491
Arno Lambert <arno.lambert@gmail.com>
2014-09-18 16:56:40 +0200

    Update bcm2708.h
    add file header
    add some more comment

--- bcm2708.h
+++ bcm2708.h
@@ -1,3 +1,12 @@
+/**
+ * header file needed to access the register of the RasPi
+ * will be needed by the all the rest
+ * this is based on the work of Pietjan VDM
+ * see : http://www.pieter-jan.com/node/15
+ * 
+ * @author Arno Lambert <arno.lambert@gmail.com>
+ */
+ 
 #include <stdio.h>
  
 #include <sys/mman.h>
@@ -13,7 +22,7 @@
  
 #define BLOCK_SIZE 		(4*1024)
  
-// IO Acces
+//structure needed to access different sorts of peripherals
 struct bcm2835_peripheral {
     unsigned long addr_p;
     int mem_fd;
@@ -21,6 +30,8 @@ struct bcm2835_peripheral {
     volatile unsigned int *addr;
 };
  
+//I think this is not correct, should not be in header....
 struct bcm2835_peripheral gpio = {GPIO_BASE};
- 
+
+//no idea what exactly this does....
 extern struct bcm2835_peripheral gpio;  // They have to be found somewhere, but can't be in the header
