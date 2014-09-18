485a9a4c1f4fc533c497106757a475c229507c4c
Arno Lambert <arno.lambert@gmail.com>
2014-09-18 17:04:23 +0200

    Update bcm2708.h

--- bcm2708.h
+++ bcm2708.h
@@ -24,6 +24,20 @@
  
 #define BLOCK_SIZE 		(4*1024)
  
+//define some macros
+//always use INP_GPIO(x) before using OUT_GPIO(x)
+//define pin g as input pin
+#define INP_GPIO(g)   *(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
+//define pin g as output pin
+#define OUT_GPIO(g)   *(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3))
+ 
+//sets bits which are 1 ignores bits which are 0
+#define GPIO_SET  *(gpio.addr + 7)  
+//clears bits which are 1 ignores bits which are 0
+#define GPIO_CLR  *(gpio.addr + 10) 
+ 
+#define GPIO_READ(g)  *(gpio.addr + 13) &= (1<<(g))
 //structure needed to access different sorts of peripherals
 struct bcm2835_peripheral {
     unsigned long addr_p;
