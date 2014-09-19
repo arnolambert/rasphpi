59488ceaeb4bced66efdbad3090da5e24d6880a5
Arno Lambert <arno.lambert@gmail.com>
2014-09-19 10:22:46 +0200

    formatting of the code, adding more comments

--- rasphpi.h
+++ rasphpi.h
@@ -25,22 +25,32 @@
  
 #define BLOCK_SIZE 		(4*1024)
  
-//define some macros
-//always use INP_GPIO(x) before using OUT_GPIO(x)
+/*
+ * define some macros
+ * always use INP_GPIO(x) before using OUT_GPIO(x)
+ */
+
 //define pin g as input pin
 #define INP_GPIO(g)   *(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
+
 //define pin g as output pin
 #define OUT_GPIO(g)   *(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
+
+//
 #define SET_GPIO_ALT(g,a) *(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3))
  
 //sets bits which are 1 ignores bits which are 0
 #define GPIO_SET  *(gpio.addr + 7)  
+
 //clears bits which are 1 ignores bits which are 0
 #define GPIO_CLR  *(gpio.addr + 10) 
- 
+
+//
 #define GPIO_READ(g)  *(gpio.addr + 13) &= (1<<(g))
 
-//structure needed to access different sorts of peripherals
+/*
+ * structure needed to access different sorts of peripherals
+ */
 struct bcm2835_peripheral {
     unsigned long addr_p;
     int mem_fd;
@@ -48,7 +58,10 @@ struct bcm2835_peripheral {
     volatile unsigned int *addr;
 };
 
-//define some functions
+/*
+ * define some functions
+ * these will be implemented in rasphpi.c
+ */
 int map_peripheral(struct bcm2835_peripheral *p);
 void unmap_peripheral(struct bcm2835_peripheral *p);
 
